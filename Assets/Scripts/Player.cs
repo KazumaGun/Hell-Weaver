ba2cda2c354ef399c1a5882106f3ef6c87f5243f
KazumaGun <KazumaGun@users.noreply.github.com>
2018-09-14 20:59:17 -0700

    Added background, player, and mouse clicking/movement.
    movement does not yet work.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿//KAZ WALSH\\
+//PLAYER SCRIPT\\
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D playerRigidBody2D;
+    public float playerSpeed = 2.0f;
+
+    public Vector3 moving;
+
+	// Use this for initialization
+	void Start ()
+    {
+        moving = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            moving = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moving.z = transform.position.z;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, moving, playerSpeed * Time.deltaTime);
+    }
+}
